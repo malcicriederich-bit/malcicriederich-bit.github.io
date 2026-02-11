265b743bc02995626cc0437cfd26bdcebddb7d25
TeamMalcic <malcic.riederich@gmail.com>
2026-02-11 17:08:07 +0100

    fix base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://malcicriederich-bit.github.io/',
-  base: '/my-repo/',
+  base: '/',
   output: 'static',
   adapter: github(),
 });
