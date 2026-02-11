1207776aabc159798028b5c66c260234eae3984d
TeamMalcic <malcic.riederich@gmail.com>
2026-02-11 17:15:01 +0100

    remove wrong github adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from 'astro/config';
-import github from '@astrojs/github-pages';
 
 export default defineConfig({
   site: 'https://malcicriederich-bit.github.io',
   output: 'static',
-  adapter: github(),
 });
